cc021b7677241ec8ded994c630806ec2acee864a
daxinhuangye <43943634@qq.com>
2018-02-10 08:14:07 +0800

    update

--- routers/router.go
+++ routers/router.go
@@ -1,14 +1,46 @@
 package routers
 
 import (
-	"Traffic/controllers"
+	_ "Traffic/controllers"
+	"Traffic/controllers/admin"
 	"github.com/astaxie/beego"
 )
 
 func init() {
 
-	beego.Router("/", &controllers.IndexController{})
+	//beego.Router("/", &controllers.IndexController{})
 
-	beego.AutoRouter(&controllers.IndexController{})
+	//beego.AutoRouter(&controllers.IndexController{})
+
+	/*********************系统路由********************************/
+
+	beego.Router("/", &admin.IndexController{})
+
+	// 登录
+	beego.AutoPrefix("admin", &admin.LoginController{})
+
+	// 节点
+	beego.AutoPrefix("admin", &admin.NodeController{})
+
+	// 模块管理
+	beego.AutoPrefix("admin", &admin.ModeController{})
+
+	// 管理员
+	beego.AutoPrefix("admin", &admin.AdminController{})
+
+	// 角色
+	beego.AutoPrefix("admin", &admin.RoleController{})
+
+	// ip屏蔽
+	beego.AutoPrefix("admin", &admin.IpbanController{})
+
+	// 日志
+	beego.AutoPrefix("admin", &admin.LogsController{})
+
+	//公共
+	beego.AutoPrefix("admin", &admin.PublicController{})
+
+	//Uedit富文本编辑器
+	beego.AutoPrefix("admin", &admin.UeditorController{})
 
 }
